22de253f6c664aacf7aadb52e578fe3d82b0d2c1
Manojtj8055 <manutj805517@gmail.com>
2023-07-02 14:14:01 -0700

    sqll

---
+++ 1_sql.sql
@@ -0,0 +1,177 @@
+CREATE database car;
+use car;
+CREATE TABLE car (
+   Thar varchar(50),
+   ferrari varchar(50),
+   compass varchar(50),
+   crysta varchar(50),
+   Koenseggera varchar(50),
+   Mustang varchar(50),
+   kuv varchar(50),
+   jimmy varchar(50),
+   Tuv varchar(50),
+   xuv varchar(50)
+);
+
+
+
+
+
+
+
+CREATE database bike;
+use bike;
+CREATE TABLE bike (
+   Gt650 varchar(50),
+   R1 varchar(50),
+   S1000RR varchar(50),
+   Z900 varchar(50),
+   CT100 varchar(50),
+   Pulsar varchar(50),
+   Aprilia varchar(50),
+   RX100 varchar(50),
+   Passion  varchar(50),
+   Bullet350 varchar(50)
+
+);
+
+
+
+
+
+
+
+CREATE database freinds;
+use friends;
+CREATE TABLE friends (
+   malli varchar(50),
+   srujan varchar(50),
+   minta varchar(50),
+   arjun varchar(50),
+   akash varchar(50),
+   dhruv varchar(50),
+   hari varchar(50),
+   gunda varchar(50),
+   darshu varchar(50),
+   shelly varchar(50)
+
+);
+
+
+
+
+
+
+
+CREATE database devices;
+use devices;
+CREATE TABLE devices (
+   oneplus varchar(50),
+   iPhone varchar(50),
+   oppo varchar(50),
+   vivo varchar(50),
+   lenovo varchar(50),
+   intex varchar(50),
+   redmi varchar(50),
+   samsung varchar(50),
+   coolpad varchar(50),
+   jio varchar(50)
+
+);
+
+
+
+
+
+
+
+CREATE database drink;
+use drink;
+CREATE TABLE drink (
+   pepsi varchar(50),
+   dew varchar(50),
+   cococola varchar(50),
+   thumsup varchar(50),
+   sting varchar(50),
+   maaza varchar(50),
+   frooti varchar(50),
+   smoodh varchar(50),
+   storm varchar(50),
+   jimmycocktail varchar(50)
+
+);
+
+
+
+
+
+
+
+
+CREATE database city;
+use city;
+CREATE TABLE city (
+   shimoga varchar(50),
+   banglore varchar(50),
+   hubli varchar(50),
+   durga varchar(50),
+   shikaripura varchar(50),
+   haveri varchar(50),
+   hassan varchar(50),
+   chickballapura varchar(50),
+   belgaum varchar(50),
+   dharwad varchar(50)
+
+);
+use bike ;
+SELECT 
+    *
+FROM
+    bike;
+
+
+
+
+
+
+CREATE database state;
+use state;
+CREATE TABLE state (
+   Karntaka varchar(50),
+   UP varchar(50),
+   MP varchar(50),
+   TN varchar(50),
+   Kerala varchar(50),
+   Bihar varchar(50),
+   gujrat varchar(50),
+   MAharashtra varchar(50),
+   rajasthan varchar(50),
+   kashmir varchar(50)
+
+);
+
+
+
+
+
+
+
+CREATE database sports;
+use sports;
+CREATE TABLE sports (
+   cricket varchar(50),
+   football varchar(50),
+   tennis varchar(50),
+   basketball varchar(50),
+   highjump varchar(50),
+   shotput varchar(50),
+   kabbadi varchar(50),
+   koko varchar(50),
+   volley varchar(50),
+   throwball varchar(50)
+
+);
+use car ;
+SELECT * FROM car ;
+
+
